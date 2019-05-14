3e2234c1845d671771a0e8bae0494894bfdddad1
yamazaki8934 <jdmvgaj@gmail.com>
2019-05-14 15:09:45 +0900

    Initial Commit

---
+++ NewsAppMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAppMVVM
+//
+//  Created by 山崎浩毅 on 2019/05/14.
+//  Copyright © 2019年 山崎浩毅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
