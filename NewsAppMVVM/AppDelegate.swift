aece5a0b0394c693c48713631e6b9d9e718d4826
山崎浩毅 <yamazakihiroki@yamazakihiroshitakeshi-no-MacBook-Pro.local>
2019-05-14 15:34:06 +0900

    first commit

--- NewsAppMVVM/AppDelegate.swift
+++ NewsAppMVVM/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().barTintColor = UIColor(displayP3Red: 142/255, green: 68/255, blue: 173/255, alpha: 1.0)
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
         return true
     }
 
---
+++ NewsAppMVVM/Controllers/NewsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsTableViewController.swift
+//  NewsAppMVVM
+//
+//  Created by 山崎浩毅 on 2019/05/14.
+//  Copyright © 2019 山崎浩毅. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class NewsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+}
